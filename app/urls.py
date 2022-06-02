5f754ce3429314cb481e521f08797e583e6d5d4b
Ronie Oljemark <ronie.oljemark@student.careeria.fi>
2022-06-02 15:30:40 +0300

    Toisen päivän jutut

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
 
-from .views import landingview, productlistview, supplierlistview
+from .views import landingview, supplierlistview, addsupplier, confirmdeletesupplier, deletesupplier, searchsuppliers
+from .views import productlistview, addproduct, productsfiltered, editproductget, editproductpost
 
 urlpatterns = [
     path('', landingview),
 
     # Products url´s
     path('products/', productlistview),
+    path('add-product/', addproduct),
+    path('products-by-supplier/<int:id>/', productsfiltered),
+    path('edit-product-get/<int:id>/', editproductget),
+    path('edit-product-post/<int:id>/', editproductpost), 
 
     # Supplier url´s
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
+    path('delete-supplier/<int:id>/', deletesupplier),
+    path('confirm-delete-supplier/<int:id>/', confirmdeletesupplier),
+    path('search-suppliers/', searchsuppliers),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Supplier, Product
 
 def landingview(request):
@@ -6,10 +6,67 @@ def landingview(request):
 
 def productlistview(request):
     productlist = Product.objects.all()
-    context ={'products': productlist}
+    supplierlist = Supplier.objects.all()
+    context ={'products': productlist, 'suppliers': supplierlist}
     return render(request, 'productlist.html', context)
+
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['packagesize']
+    c = request.POST['unitprice']
+    d = request.POST['unitsinstock']
+    e = request.POST['supplier']
+    
+    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
+    return redirect(request.META['HTTP_REFERER'])    
+
+def productsfiltered(request, id):
+    productlist = Product.objects.all()
+    supplierlist = Supplier.objects.all()
+    supplier = Supplier.objects.get(id = id)
+    filteredproducts = productlist.filter(supplier = id)
+    context ={'products': filteredproducts, 'suppliers': supplierlist, "supplier" : supplier}
+    return render (request,"filteredproductlist.html",context)    
+
+def editproductget(request, id):
+    product = Product.objects.get(id = id)
+    context = {'product': product}
+    return render (request,"editproduct.html",context)
+
+
+def editproductpost(request, id):
+    item = Product.objects.get(id = id)
+    item.unitprice = request.POST['unitprice']
+    item.unitsinstock = request.POST['unitsinstock']
+    item.save()
+    return redirect(productlistview)    
 
 def supplierlistview(request):
     supplierlist = Supplier.objects.all()
     context ={'suppliers': supplierlist}
     return render(request, 'supplierlist.html', context)
+
+def addsupplier(request):
+    a = request.POST['companyname']
+    b = request.POST['contactname']
+    c = request.POST['address']
+    d = request.POST['phone']
+    e = request.POST['email']
+    f = request.POST['country']
+    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+def deletesupplier(request, id):
+    Supplier.objects.get(id = id).delete()
+    return redirect(supplierlistview)
+
+def confirmdeletesupplier(request, id):
+    supplier = Supplier.objects.get(id = id)
+    context = {'supplier': supplier}
+    return render (request,"confirmdeletesupplier.html",context)    
+
+def searchsuppliers(request):
+    search = request.POST['search']
+    filtered = Supplier.objects.filter(companyname__icontains=search)
+    context = {'suppliers': filtered}
+    return render (request,"searchsuppliers.html",context)
